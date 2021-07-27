9eb5c9da4a2de41c42ca12ddbd26e689b94fbd58
huramba <huramba@gmail.com>
2021-07-27 23:06:48 +0500

    parametrized node workers count

--- src/train.sh
+++ src/train.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-python3 -m torch.distributed.launch --nproc_per_node 2 train.py --img $IMG_SIZE --batch $BATCH_SIZE --epochs $EPOCHS --data ${CONFIG_PATH:-/srv/dataset/config.yaml} --iou-thres $IOU --weights $TL_WEIGHTS --cache
+python3 -m torch.distributed.launch --nproc_per_node ${TDRPROC:-4} train.py --img $IMG_SIZE --batch $BATCH_SIZE --epochs $EPOCHS --data ${CONFIG_PATH:-/srv/dataset/config.yaml} --iou-thres $IOU --weights $TL_WEIGHTS --cache
 ls -la /srv/runs/train/exp
